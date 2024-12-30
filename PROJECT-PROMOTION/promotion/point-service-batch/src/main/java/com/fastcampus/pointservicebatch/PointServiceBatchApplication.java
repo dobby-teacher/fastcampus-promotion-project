fda178af8f4227d3e1199fe8d1c17dcac50a73cf
dobby-teacher <admin@dobby.work>
2024-12-30 21:10:52 +0900

    runner 설정 추가

--- PROJECT-PROMOTION/promotion/point-service-batch/src/main/java/com/fastcampus/pointservicebatch/PointServiceBatchApplication.java
+++ PROJECT-PROMOTION/promotion/point-service-batch/src/main/java/com/fastcampus/pointservicebatch/PointServiceBatchApplication.java
@@ -1,11 +1,39 @@
 package com.fastcampus.pointservicebatch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class PointServiceBatchApplication {
+
+    private final JobLauncher jobLauncher;
+    private final Job pointBalanceSyncJob;
+
+    public PointServiceBatchApplication(JobLauncher jobLauncher, Job pointBalanceSyncJob) {
+        this.jobLauncher = jobLauncher;
+        this.pointBalanceSyncJob = pointBalanceSyncJob;
+    }
+
     public static void main(String[] args) {
-        System.exit(SpringApplication.exit(SpringApplication.run(PointServiceBatchApplication.class, args)));
+        SpringApplication.run(PointServiceBatchApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner() {
+        return args -> {
+            jobLauncher.run(
+                    pointBalanceSyncJob,
+                    new JobParametersBuilder()
+                            .addLong("timestamp", System.currentTimeMillis()) // JobParameters 예시
+                            .toJobParameters()
+            );
+        };
     }
 }
